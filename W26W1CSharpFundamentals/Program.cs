39a67609fec0b1da541d8a47ffcafe6fbc485596
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-09 09:28:12 -0500

    variables

--- W26W1CSharpFundamentals/Program.cs
+++ W26W1CSharpFundamentals/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        static int z;
+
         static void Main(string[] args)
         {
-            
+            int @double = 5;
+
+            int i = 5;
+            Console.WriteLine(z);
+
+            double d = 5.5;
+
+            float f = 5.5F;
+            decimal m = 5.5M;
+            long l = 3454353L;
         }
     }
 }
